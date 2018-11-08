db83a506918bae9221a4a290817c1d7caa21c124
Punctum099 <wjstjddlr1@naver.com>
2018-11-08 16:03:45 +0900

    글쓰기 기능 추가 + 글 수정 기능 절반정도 추가 (보이는 부분(contents_view.jsp)에서 바로 수정 → 보이는 부분과 바꾸는 부분을 분리할 예정)

--- gaesipan/src/frontcontroller/BFrontController.java
+++ gaesipan/src/frontcontroller/BFrontController.java
@@ -78,11 +78,9 @@ public class BFrontController extends HttpServlet {
 			command.execute(request, response);
 			viewPage = "list.do";
 		}else if(com.equals("/write_view.do")) {
-			viewPage = "write_view.jsp";
-		}else if(com.equals("/write.do")) {
 			command = new BWriteCommand();
 			command.execute(request, response);
-			viewPage = "list.do";
+			viewPage = "write_view.jsp";
 		}
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
